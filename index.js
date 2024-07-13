fa93110de9258e793199e7af0ecb4d02f2e1f4df
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-13 15:41:00 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+    console.log(bestCustomer)
+}
+console.log(bestCustomer)
+setBestCustomer(); 
+// console.log(bestCustomer)
+
+function overwriteBestCustomer(newBestCustomer) {
+    newBestCustomer = 'maybe bob';
+    bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'mr bob'
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'mr bob'
+    leastFavoriteCustomer = 'jane'
+}
+
+changeLeastFavoriteCustomer()
+
